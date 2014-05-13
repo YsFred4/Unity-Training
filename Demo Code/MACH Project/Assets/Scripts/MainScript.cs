e92c2be0e25cb7102fb17993f217a8be82b1adf5
ysfred4 <steve.maier@gmail.com>
2014-05-13 13:44:17 -0400

    Added demo project

---
+++ Demo Code/MACH Project/Assets/Scripts/MainScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScript : MonoBehaviour {
+
+	public GameObject BoxGameObject;
+
+	// Use this for initialization
+	void Start () {
+	
+		Instantiate(BoxGameObject, new Vector3(0,3,1), Quaternion.identity);
+		Instantiate(BoxGameObject, new Vector3(1,2,0), Quaternion.identity);
+		Instantiate(BoxGameObject, new Vector3(2,3,0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Demo Code/MACH Project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float moveSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float fwd = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+		float side = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+		transform.Translate(side, 0, fwd);
+	}
+}
